0ec7cdffd8f585a10f85e47c26cd6a3bb5ff8e4b
schintak <sarojchin@gmail.com>
2025-05-26 20:08:05 -0700

    temporarily allow all origins

--- src/app.py
+++ src/app.py
@@ -12,7 +12,8 @@ app = FastAPI()
 # Allow React frontend to call API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://htsus-classifier.vercel.app"],  # React dev server origin
+    # allow_origins=["https://htsus-classifier.vercel.app"],  # React dev server origin
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
